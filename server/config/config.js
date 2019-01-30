c3e9b10d0901ccd284677231a0d1e199b82cc17d
Luis Ayard <draya_1@hotmail.com>
2019-01-30 08:56:57 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://user-cafe:usuariocafe1@ds157559.mlab.com:57559/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
